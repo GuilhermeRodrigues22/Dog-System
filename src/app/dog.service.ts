331ad2daf3b76cf0b253b4cb18a65e6e3503c459
KTROXZ <jm4536@outlook.com>
2023-04-25 16:58:21 -0300

    Dog System finalizado

---
+++ src/app/dog.service.ts
@@ -0,0 +1,19 @@
+import { Dog } from './dog';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DogService {
+
+  constructor( private http: HttpClient) {
+
+   }
+
+   getDog() : Observable<Dog> {
+    return this.http.get<Dog>("https://dog.ceo/api/breeds/image/random");
+
+   }
+}
